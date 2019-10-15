86b1f7436232eacb2cbcba67e13885b3b8ec8b43
John Z <jczerbe14@icloud.com>
2019-10-15 16:08:33 -0600

    added content back to db

--- db/db.js
+++ db/db.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+});
 
 mongoose.connection.on('connected', () => {
     console.log('MONGOOSE IS CONNECTED')
