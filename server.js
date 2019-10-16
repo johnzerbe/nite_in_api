5c562e07a3d6a96a9d3d84bac987626112104e27
John Z <jczerbe14@icloud.com>
2019-10-16 12:56:28 -0600

    put 200 back in

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ const corsOptions = {
     // origin: 'http://localhost:3000', // when react app is deployed, this is where the address goes
     origin: 'https://nitein3.herokuapp.com/',
     credentials: true, // allows cookies to be sent with requests from the client (session cookie)
-    optionsSuccessStatus: 204
+    optionsSuccessStatus: 200
 }
 
 app.use(cors(corsOptions));
